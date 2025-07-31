5882df80d71c1a6a44a429e6f6d7acba5efc3b7c
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-31 11:53:01 +0200

    try out new syntax
    If I'm making a real library, I can invest a bit more in the macro than
    I did in my blog.

--- test/io/github/gaverhae/clonad_test.clj
+++ test/io/github/gaverhae/clonad_test.clj
@@ -1,6 +1,33 @@
 (ns io.github.gaverhae.clonad-test
   (:require [clojure.test :refer [deftest is]]
-            [io.github.gaverhae.clonad :as t]))
+            [io.github.gaverhae.clonad :as t :refer [match mdo monad]]))
 
-(deftest truism
-  (is (= 1 1)))
+;; The do-nothing monad, just testing the syntax
+
+(defn run-plain
+  [ma]
+  (match ma
+    [:pure v] v
+    [:bind ma f] (let [a (run-plain ma)]
+                   (run-plain (f a)))))
+
+(deftest old-syntax
+  (is (= 12
+         (run-plain
+           (mdo [a [:pure 3]
+                 b [:pure 4]
+                 _ [:pure (* a b)]])))))
+
+(deftest new-syntax
+  (is (= '[:bind [:pure 3] (clojure.core/fn [a] [:bind [:pure 4] (clojure.core/fn [b] [:pure (* a b)])])]
+         (macroexpand
+           '(io.github.gaverhae.clonad/monad
+              a :<< [:pure 3]
+              b :<< [:pure 4]
+              [:pure (* a b)]))))
+  (is (= 12
+         (run-plain
+           (monad
+             a :<< [:pure 3]
+             b :<< [:pure 4]
+             [:pure (* a b)])))))
